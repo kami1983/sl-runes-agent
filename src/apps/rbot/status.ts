3c0d50e772c66520dea8d3c2be3bde541bdb3e29
kami1983 <kami@cancanyou.com>
2024-06-07 10:26:21 +0800

    init version.

--- src/apps/rbot/status.ts
+++ src/apps/rbot/status.ts
@@ -1,3 +1,4 @@
+import { number } from 'bitcoinjs-lib/src/script';
 import Knex from 'knex';
 
 /*
@@ -252,9 +253,14 @@ export const deleteSlLocation = async (pool: Knex.Knex, rid: number) => {
 export const getSLLocationList = async (pool: Knex.Knex, minutes: number): Promise<SlLocation[]> => {
 
   console.log('minutes - ', minutes)
-  const startTime = new Date((new Date()).getTime() - minutes * 60 * 1000)
 
+  if(minutes < 1) {
+    minutes = 60
+  }
+
+  const startTime = new Date((new Date()).getTime() - minutes * 60 * 1000)
   console.log('startTime: ', startTime)
+
   return await pool('sl_location')
     .where('update_time', '>=', startTime.toISOString())
     .orderBy('update_time', 'desc')
